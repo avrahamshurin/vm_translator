e0d081ac8241dfb8008014da946381162264ff05
avrahamshurin <50734267+avrahamshurin@users.noreply.github.com>
2019-05-27 21:56:44 +0300

    work in progress

--- first_try.py
+++ first_try.py
@@ -1,6 +1,8 @@
-a= "eq     constant       8// sdfgd"
+a= "push    pointer       1// sdfgd"
+file_name = "temp"
 b = a.split("//")[0].split(" ")
 c = [item for item in b if item != ""]
+memory_dic ={"local":"LCL","argument":"ARG","this":"THIS","that":"THAT","0":"THIS","1":"THAT"}
 print(b)
 print(c)
 print("// " + " ".join(c))
@@ -9,6 +11,24 @@ num = 0
 if c[0] == "push":
     if c[1] == "constant":
         print("@{}\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1".format(c[2]))
+    elif c[1] == "local" or c[1] =="argument" or c[1] =="this" or c[1]=="that":
+        print("@{}\nD=A\n@{}\nA=M\nM=D+M\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1".format(c[2],memory_dic[c[1]]))
+    elif c[1] =="static":
+        print("@{}.{}\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1".format(file_name,c[2]))
+    elif c[1] == "temp":
+        print("@{}\nD=A\n@5\nA=D+A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1".format(c[2]))
+    elif c[1] =="pointer":
+        print("@{}\nA=M\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1".format(memory_dic[c[2]]))
+
+if c[0] == "pop":
+    if c[1] == "local" or c[1] =="argument" or c[1] =="this" or c[1]=="that":
+        print("@{}\nD=A\n@{}\nD=D+M\n@address\nM=D\n@SP\nM=M-1\nA=M\nD=M\n@address\nA=M\nM=D".format(c[2],memory_dic[c[1]]))
+    elif c[1] =="static":
+        print("@SP\nM=M-1\nA=M\nD=M\n@{}.{}\nM=D".format(file_name,c[2]))
+    elif c[1] == "temp":
+        print("@{}\nD=A\n@5\nD=D+A\n@address\nM=D\n@SP\nM=M-1\nA=M\nD=M\n@address\nA=M\nM=D".format(c[2]))
+    elif c[1] == "pointer":
+        print("@SP\nM=M-1\nA=M\D=M@{}\nA=M\nM=D".format(memory_dic[c[2]]))
 
 
 
@@ -20,13 +40,13 @@ elif c[0]=="sub":
 elif c[0] == "neg":
     print("@SP\nA=M-1\nM=-M")
 elif c[0] == "eq":
-    print("@SP\nM=M-1\nA=M\nD=M\nA=A-1\nD=D-M\n@one{}\nD;JEQ\n@SP\nA=M-1\nM=0\n@cont{}\n0;JMP\n(one{})\n@SP\nA=M-1\nM=-1\n(cont{})".format(num,num,num,num))
+    print("@SP\nM=M-1\nA=M\nD=M\nA=A-1\nD=D-M\n@one{0}\nD;JEQ\n@SP\nA=M-1\nM=0\n@cont{0}\n0;JMP\n(one{0})\n@SP\nA=M-1\nM=-1\n(cont{0})".format(num))
     num += 1
 elif c[0] == "gt":
-    print("@SP\nM=M-1\nA=M\nD=M\nA=A-1\nD=D-M\n@one{}\nD;JLT\n@SP\nA=M-1\nM=0\n@cont{}\n0;JMP\n(one{})\n@SP\nA=M-1\nM=-1\n(cont{})".format(num,num,num,num))
+    print("@SP\nM=M-1\nA=M\nD=M\nA=A-1\nD=D-M\n@one{0}\nD;JLT\n@SP\nA=M-1\nM=0\n@cont{0}\n0;JMP\n(one{0})\n@SP\nA=M-1\nM=-1\n(cont{0})".format(num))
     num += 1
 elif c[0] == "lt":
-    print("@SP\nM=M-1\nA=M\nD=M\nA=A-1\nD=D-M\n@one{}\nD;JGT\n@SP\nA=M-1\nM=0\n@cont{}\n0;JMP\n(one{})\n@SP\nA=M-1\nM=-1\n(cont{})".format(num,num,num,num))
+    print("@SP\nM=M-1\nA=M\nD=M\nA=A-1\nD=D-M\n@one{0}\nD;JGT\n@SP\nA=M-1\nM=0\n@cont{0}\n0;JMP\n(one{0})\n@SP\nA=M-1\nM=-1\n(cont{0})".format(num))
     num += 1
 elif c[0] == "and":
     print("@SP\nM=M-1\nA=M\nD=M\nA=A-1\nM=D&M")
@@ -34,6 +54,11 @@ elif c[0] == "and":
     print("@SP\nM=M-1\nA=M\nD=M\nA=A-1\nM=D|M")
 elif c[0] == "not":
     print("@SP\nA=M-1\nM=!M")
+
+
+
+
+
 file = open("result.txt", "w")
 file.write("// " + " ".join(c) + "\n")
 
